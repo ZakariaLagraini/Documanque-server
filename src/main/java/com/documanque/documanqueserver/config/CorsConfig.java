eddf598bd8256615dac49d9077bbc4b143597ceb
Zakaria Lagraini <zakarialaganimes@gmail.com>
2024-07-22 11:45:42 +0100

    add the corsconf

---
+++ src/main/java/com/documanque/documanqueserver/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.documanque.documanqueserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://documanque-server.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
